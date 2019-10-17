e5e63441fb4c3ac8e22e59b924c8559c32a3240c
André <andrescrich@hotmail.com>
2019-10-17 12:20:01 +0200

    health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public abstract class Health : MonoBehaviour
+{
+    [SerializeField] protected float currentHealth;
+    [SerializeField] protected float maxHealth;
+
+    public float CurrentHealth
+    {
+        get => currentHealth;
+        set => currentHealth = value > maxHealth ? maxHealth : value;
+    }
+
+    public float MaxHealth
+    {
+        get => maxHealth;
+        set
+        {
+            if (value < 0)
+                maxHealth = 0;
+            else
+                maxHealth = value;
+        }
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+    
+    public void TakeDamage(float dmg)
+    {
+        currentHealth -= dmg;
+    }
+}
